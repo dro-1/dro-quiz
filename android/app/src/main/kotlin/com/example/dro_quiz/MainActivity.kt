79d7f2c79db0ef89b36193a74acde469469d28a0
dro-1 <lorddro1532@gmail.com>
2021-03-20 16:07:04 +0100

    App Init with few changes

---
+++ android/app/src/main/kotlin/com/example/dro_quiz/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dro_quiz
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  void answerQuestion() {
+    print('Answer Question');
+    questionIndex++;
+  }
+
+  var questionIndex = 0;
+
+  Widget build(BuildContext build) {
+    var questions = [
+      'What\'s your favourite color ? ',
+      'What\'s your favourite animal'
+    ];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Text(questions[questionIndex]),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: () => answerQuestion(),
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer 3'),
+              onPressed: answerQuestion,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
